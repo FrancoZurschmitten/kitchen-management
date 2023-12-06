25b9183c5e7f7e18d7e937d2ec122f914cdeaaf6
franco <FrancoZurschmitten>
2023-12-06 11:03:42 -0300

    Create FoodBaseAbstract and Vegetable models

--- backend/foods/models.py
+++ backend/foods/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class FoodBaseAbstract(models.Model):
+    name = models.CharField(_("Nombre"), max_length=150)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Vegetable(FoodBaseAbstract):
+    pass
